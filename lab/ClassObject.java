ffd869471f86af78de677fb1e1c47f9fff66bbdd
saiqamardi16 <saiqamardia@gmail.com>
2026-06-10 16:20:17 +0600

    25-62993-2

---
+++ lab/ClassObject.java
@@ -0,0 +1,20 @@
+package lab;
+
+public class ClassObject {
+
+    public void setText(String string) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'setText'");
+    }
+
+    public void clearText() {
+
+        throw new UnsupportedOperationException("Unimplemented method 'clearText'");
+    }
+
+    public String getText() {
+    
+        throw new UnsupportedOperationException("Unimplemented method 'getText'");
+    }
+
+}
